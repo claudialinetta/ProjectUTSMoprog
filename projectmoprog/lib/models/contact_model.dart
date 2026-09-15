d8bd4bcc9d740490cd72c4272e1a3d3b4550172f
claudialinetta <claudialinetta07@gmail.com>
2026-09-15 21:55:53 +0700

    feat(models): add ContactTag and UserModel, extend ContactModel with tags

--- projectmoprog/lib/models/contact_model.dart
+++ projectmoprog/lib/models/contact_model.dart
@@ -1,10 +1,13 @@
+import 'contact_tag.dart';
+
 class ContactModel {
   final String id;
   final String name;
   final String phoneNumber;
-  final String tag;
-  final int reportCount;
+  String tag;
+  int reportCount;
   final String avatarInitial;
+  final List<ContactTag> tags;
 
   ContactModel({
     required this.id,
@@ -13,5 +16,36 @@ class ContactModel {
     required this.tag,
     required this.reportCount,
     required this.avatarInitial,
-  });
+    List<ContactTag>? tags,
+  }) : tags = tags ?? [];
+
+  void refreshHeadlineTag() {
+    if (reportCount >= 10) {
+      tag = 'Spam Likely';
+    } else if (tags.isNotEmpty) {
+      tag = tags.first.label;
+    }
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'name': name,
+    'phoneNumber': phoneNumber,
+    'tag': tag,
+    'reportCount': reportCount,
+    'avatarInitial': avatarInitial,
+    'tags': tags.map((t) => t.toJson()).toList(),
+  };
+
+  factory ContactModel.fromJson(Map<String, dynamic> json) => ContactModel(
+    id: json['id'] as String,
+    name: json['name'] as String,
+    phoneNumber: json['phoneNumber'] as String,
+    tag: json['tag'] as String,
+    reportCount: json['reportCount'] as int,
+    avatarInitial: json['avatarInitial'] as String,
+    tags: (json['tags'] as List<dynamic>? ?? [])
+        .map((t) => ContactTag.fromJson(t as Map<String, dynamic>))
+        .toList(),
+  );
 }
