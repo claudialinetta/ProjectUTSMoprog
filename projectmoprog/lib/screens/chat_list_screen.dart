ddee927872c16e47e38b4f04091e4bc30a3124e4
OwenBenedict-L <owen.lukman07@gmail.com>
2026-09-16 11:40:47 +0700

    Adding Chats to Contact

---
+++ projectmoprog/lib/screens/chat_list_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import '../services/contact_service.dart';
+import '../models/contact_model.dart';
+import 'chat_screen.dart';
+
+class ChatListScreen extends StatefulWidget {
+  const ChatListScreen({super.key});
+
+  @override
+  State<ChatListScreen> createState() => _ChatListScreenState();
+}
+
+class _ChatListScreenState extends State<ChatListScreen> {
+  final ContactService _service = ContactService();
+  List<ContactModel> _chatHistory = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchChatHistory();
+  }
+
+  Future<void> _fetchChatHistory() async {
+    final contacts = await _service.getContacts();
+    setState(() {
+      _chatHistory = contacts;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Riwayat Chat"),
+      ),
+      body: _isLoading 
+          ? const Center(child: CircularProgressIndicator()) 
+          : ListView.builder(
+              itemCount: _chatHistory.length,
+              itemBuilder: (context, index) {
+                final contact = _chatHistory[index];
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.blue.shade100,
+                    child: Text(contact.avatarInitial),
+                  ),
+                  title: Text(contact.name, style: const TextStyle(fontWeight: FontWeight.bold)),
+                  subtitle: const Text("Pesan terakhir di sini..."),
+                  trailing: const Text("14:00", style: TextStyle(color: Colors.grey, fontSize: 12)),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChatScreen(contact: contact),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+    );
+  }
+}
---
+++ projectmoprog/lib/screens/nav_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'chat_list_screen.dart';
+
+class NavScreen extends StatefulWidget {
+  const NavScreen({super.key});
+
+  @override
+  State<NavScreen> createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomeScreen(),
+    const ChatListScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contacts),
+            label: 'Kontak',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chats',
+          ),
+        ],
+      ),
+    );
+  }
+}
