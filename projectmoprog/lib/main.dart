42a5b9319a30cfcae983ed7501238e337b96b787
claudialinetta <claudialinetta07@gmail.com>
2026-09-16 11:52:29 +0700

    refactor(app): route through splash screen for auth flow

--- projectmoprog/lib/main.dart
+++ projectmoprog/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projectmoprog/screens/splash_screen.dart';
 
 import 'theme/app_theme.dart';
 import 'screens/home_screen.dart';
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'GetContact Clone',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      home: const HomeScreen(),
+      home: const SplashScreen(),
     );
   }
 }
